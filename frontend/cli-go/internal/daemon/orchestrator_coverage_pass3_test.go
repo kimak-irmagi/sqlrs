c9f49b084a9f141c182f06216467af88a4956d84
Anton Zlygostev <zlygostev@hotmail.com>
2026-08-22 22:05:09 +0700

    Stabilize Windows daemon startup test

--- frontend/cli-go/internal/daemon/orchestrator_coverage_pass3_test.go
+++ frontend/cli-go/internal/daemon/orchestrator_coverage_pass3_test.go
@@ -244,7 +244,7 @@ func writeHealthyDaemonScript(t *testing.T, dir, endpoint, instanceID, token str
 			"shift\r\n" +
 			"goto parse\r\n" +
 			":parsed\r\n" +
-			"powershell -NoProfile -Command \"Start-Sleep -Milliseconds 200\"\r\n" +
+			"ping -n 2 127.0.0.1 >nul\r\n" +
 			fmt.Sprintf("> \"%%state%%\" echo {\"endpoint\":\"%s\",\"instanceId\":\"%s\",\"authToken\":\"%s\"}\r\n", endpoint, instanceID, token) +
 			"timeout /t 2 /nobreak >nul\r\n"
 		if err := os.WriteFile(path, []byte(content), 0o700); err != nil {
